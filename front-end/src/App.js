c60f2224f1a33d0ac31027547b2c1fe19653666e
Abrar Zaman <abrarzaman2003@gmail.com>
2022-02-05 16:37:11 -0600

    got the recorder kinda working

---
+++ front-end/src/App.js
@@ -0,0 +1,11 @@
+import RecorderJSDemo from "./components/recorder";
+
+function App() {
+  return (
+    <div className="App">
+      <RecorderJSDemo></RecorderJSDemo>
+    </div>
+  );
+}
+
+export default App;
