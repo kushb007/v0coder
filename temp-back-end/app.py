0d684af5f3c83e9d7c0d8bb716eeb440753e6036
Abrar Zaman <abrarzaman2003@gmail.com>
2022-02-05 20:05:34 -0600

    check if this works kush

---
+++ temp-back-end/app.py
@@ -0,0 +1,26 @@
+import requests
+filename = "./b.m4a"
+def read_file(filename, chunk_size=5242880):
+    with open(filename, 'rb') as _file:
+        while True:
+            data = _file.read(chunk_size)
+            if not data:
+                break
+            yield data
+
+headers = {'authorization': "d5086660044c456f8645a3a38e517c0b"} 
+response = requests.post('https://api.assemblyai.com/v2/upload',
+                        headers=headers,
+                        data=read_file(filename))
+
+print(response.json())
+
+endpoint = "https://api.assemblyai.com/v2/transcript"
+json = { "audio_url": response.json()['upload_url'] }
+headers = {
+    "authorization": "d5086660044c456f8645a3a38e517c0b",
+    "content-type": "application/json"
+}
+response = requests.post(endpoint, json=json, headers=headers)
+print(response.json())
+
